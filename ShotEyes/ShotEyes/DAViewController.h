9a8b3af828b0599ab116e329b8b22d188d6a14db
h_li <h_li@dreamats.co.jp>
2013-12-27 16:41:56 +0800

    first commit

---
+++ ShotEyes/ShotEyes/DAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DAViewController.h
+//  ShotEyes
+//
+//  Created by Antony on 13-12-27.
+//  Copyright (c) 2013年 DAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAViewController : UIViewController
+
+@end
---
+++ ShotEyes/ShotEyes/DAViewController.m
@@ -0,0 +1,29 @@
+//
+//  DAViewController.m
+//  ShotEyes
+//
+//  Created by Antony on 13-12-27.
+//  Copyright (c) 2013年 DAC. All rights reserved.
+//
+
+#import "DAViewController.h"
+
+@interface DAViewController ()
+
+@end
+
+@implementation DAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
