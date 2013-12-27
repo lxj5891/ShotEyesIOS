3e4d8e85e6765cc7bd2b4b0434eec98ed2cc619b
h_li <h_li@dreamats.co.jp>
2013-12-27 16:19:03 +0800

    first commit

---
+++ ShotEyes/ShotEyes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShotEyes
+//
+//  Created by Antony on 13-12-27.
+//  Copyright (c) 2013年 DAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAppDelegate class]));
+    }
+}
